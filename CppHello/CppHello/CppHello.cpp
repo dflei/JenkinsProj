957958cbccfa15ea37150fb35a7e71a3576a2fe1
Jiajin Lei <jlei@abpeds.org>
2018-11-07 12:12:17 -0500

    Add some worlds

--- CppHello/CppHello/CppHello.cpp
+++ CppHello/CppHello/CppHello.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello world from visual studio, the great IDE! tens million developers using it" << endl;
+	cout<<"what are you going to do with it?"
     return 0;
 }
 
