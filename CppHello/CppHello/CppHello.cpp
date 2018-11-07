dbae7f46c2393a674974c024dccbe9e45e0cab73
Jiajin Lei <jlei@abpeds.org>
2018-11-07 12:17:57 -0500

    fix the bug

--- CppHello/CppHello/CppHello.cpp
+++ CppHello/CppHello/CppHello.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello world from visual studio, the great IDE! tens million developers using it" << endl;
-	cout<<"what are you going to do with it?"
+	cout << "what are you going to do with it?" << endl;
     return 0;
 }
 
